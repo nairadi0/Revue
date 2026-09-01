a1432c1a1792793565675dd843ebec5f2bf33bda
nairadi0 <nairadi@oregonstate.edu>
2026-08-31 17:31:28 -0700

    Add getting PR file and storing diffs

--- app/routers/pull_requests.py
+++ app/routers/pull_requests.py
@@ -3,7 +3,7 @@ import httpx
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .auth import get_current_user, get_valid_access_token
-from ..models import User, Repository, UserRepository, PullRequest, Status
+from ..models import User, Repository, UserRepository, PullRequest, Status, PRFile
 
 
 router = APIRouter()
@@ -60,3 +60,55 @@ async def get_prs(repo_id: int, current_user: User = Depends(get_current_user), 
                     "status" : pr.status}) 
     db.commit()
   return prs
+
+
+@router.get("/repos/{repo_id}/prs/{pr_number}/files")
+async def get_pr_files(repo_id: int, pr_number: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+   repo_access = db.query(UserRepository).filter(UserRepository.user_id == current_user.id, UserRepository.repo_id == repo_id).first()
+   if not repo_access: raise HTTPException(status_code=403, detail="You do not have access to this repository")
+   repo = db.query(Repository).filter(Repository.id == repo_id).first()
+   pr = db.query(PullRequest).filter(PullRequest.repo_id == repo_id, PullRequest.pr_number == pr_number).first()
+   if pr is None: raise HTTPException(status_code=404, detail="Pull Request not found")
+   await get_valid_access_token(current_user, db)
+   owner = repo.owner
+   name = repo.name
+   base_url = f"https://api.github.com/repos/{owner}/{name}/pulls/{pr_number}/files"
+   async with httpx.AsyncClient() as client:
+      auth_header = {"Authorization" : f"Bearer {current_user.access_token}"}
+      response = await client.get(base_url, headers=auth_header)
+      if response.status_code != 200:
+                raise HTTPException(status_code=404, detail="Cannot find files for this Pull Request")
+      pr_files = response.json()
+      files = []
+      for file in pr_files:
+          file_path = file['filename']
+          additions = file['additions']
+          deletions = file['deletions']
+          patch_text = file.get('patch', '')
+          file = db.query(PRFile).filter(PRFile.pr_id == pr.id, PRFile.file_path == file_path).first()
+          if file:
+              file.additions = additions
+              file.deletions = deletions
+              file.patch_text = patch_text
+          else: 
+              file = PRFile (
+                  pr_id = pr.id,
+                  file_path = file_path,
+                  additions = additions,
+                  deletions = deletions,
+                  patch_text = patch_text
+              )
+              db.add(file)
+              db.flush()
+          files.append({"id" : file.id,
+                        "pr_id" : file.pr_id,
+                        "file_path" : file.file_path,
+                        "additions" : file.additions,
+                        "deletions" : file.deletions,
+                        "patch_text" : file.patch_text
+                        })
+      db.commit()
+   return files
+            
+
+
